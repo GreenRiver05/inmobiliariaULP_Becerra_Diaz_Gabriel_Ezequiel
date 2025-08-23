320e8a5212909f01e82d493417d72627f269215d
greenriver05 <gabrielbd94@gmail.com>
2025-08-23 15:00:38 -0300

    Agregamos IRepositorio.cs y RepositorioBase.cs

---
+++ inmobiliariaBD/Models/IRepositorio.cs
@@ -0,0 +1,37 @@
+//Trae clases básicas del sistema como Console, DateTime, Exception, etc.
+using System;
+
+//Permite usar colecciones genéricas como List<T>, Dictionary<TKey, TValue>, etc.
+using System.Collections.Generic;
+
+//Habilita LINQ (Language Integrated Query), útil para consultar colecciones con expresiones como .Where(), .Select(), .FirstOrDefault(), etc.
+using System.Linq;
+
+//Proporciona soporte para programación asincrónica con Task, async, await.
+using System.Threading.Tasks;
+
+
+//Define el espacio de nombres propio del proyecto, agrupando tus clases e interfaces bajo una misma etiqueta lógica
+namespace Inmobiliaria_.Net_Core.Models
+
+{
+    public interface IRepositorio<T>
+    {
+        int Alta(T p);
+        int Baja(int id);
+        int Modificacion(T p);
+
+        IList<T> ObtenerTodos();
+        T ObtenerPorId(int id);
+    }
+}
+
+
+/**
+Define un contrato genérico para operaciones CRUD sobre entidades de tipo T.
+Se usa para abstraer el acceso a datos y facilitar la reutilización y el testeo.
+
+USING:
+Evitan escribir nombres largos: en vez de System.Collections.Generic.List<T>, podés escribir simplemente List<T>.
+Organizan el código: te permiten saber qué funcionalidades estás usando.
+Facilitan el mantenimiento: si ves un using System.Linq, ya sabés que probablemente hay consultas LINQ en el archivo. **/
---
+++ inmobiliariaBD/Models/RepositorioBase.cs
@@ -0,0 +1,43 @@
+//Permite acceder a la interfaz IConfiguration, que se usa para leer valores del archivo appsettings.json(como cadenas de conexión, claves, etc.).
+using Microsoft.Extensions.Configuration;
+
+//tipos básicos como Exception, DateTime, etc.
+using System;
+
+//listas, diccionarios (List<T>, Dictionary<K,V>, etc.).
+using System.Collections.Generic;
+
+//consultas sobre colecciones (Where, Select, etc.).
+using System.Linq;
+
+//soporte para programación asincrónica (Task, async, await).
+using System.Threading.Tasks;
+
+namespace Inmobiliaria_.Net_Core.Models
+{
+    public abstract class RepositorioBase
+    {
+
+        //Guarda la configuración inyectada y extrae la cadena de conexión.
+        protected readonly IConfiguration configuration;
+        protected readonly string connectionString;
+
+
+        //Constructor que recibe la configuración (por inyección de dependencias) y obtiene la cadena de conexión desde appsettings.json.
+        protected RepositorioBase(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            connectionString = configuration["ConnectionStrings:DefaultConnection"];
+            //connectionString = configuration["ConnectionStrings:MySql"];
+        }
+    }
+}
+
+
+/**
+
+Clase base para repositorios que centraliza el acceso a la cadena de conexión.
+Utiliza IConfiguration para obtener valores desde appsettings.json.
+Diseñada para ser heredada por repositorios concretos.
+
+**/
