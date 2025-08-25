4cc039f33c01050b3137f706de0c14ad81dac90d
greenriver05 <gabrielbd94@gmail.com>
2025-08-24 22:48:37 -0300

    Clases repositorios

---
+++ inmobiliariaBD/Models/Contacto.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace inmobiliariaBD.Models
+{
+    public class Contacto
+    {
+        [Key]
+        [Display(Name = "nro° interno")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "DNI")]
+        public int Dni { get; set; }
+
+        public string? Correo { get; set; }
+
+        [Display(Name = "Teléfono")]
+        public int? Telefono { get; set; }
+
+        [Display(Name = "Teléfono Secundario")]
+        public int? TelefonoSecundario { get; set; }
+
+        //[ForeignKey("Dni")]
+        [ForeignKey(nameof(Dni))]
+        [Display(Name = "Persona")]
+        public Persona Persona { get; set; }
+    }
+}
