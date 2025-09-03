21576ce5e4ab27b865d88834d5eaa64d21325db2
greenriver05 <gabrielbd94@gmail.com>
2025-09-03 16:59:44 -0300

    actualizacion en tipos de valores en BD

--- 3_inmobiliaria_lab2_plan2024.sql
+++ 3_inmobiliaria_lab2_plan2024.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-08-2025 a las 01:05:16
+-- Tiempo de generación: 03-09-2025 a las 20:41:06
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -85,6 +85,17 @@ CREATE TABLE `inquilino` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `inquilino`
+--
+
+INSERT INTO `inquilino` (`id`, `dni`, `estado`) VALUES
+(1, 50000555, 0),
+(2, 20000999, 0),
+(3, 20000313, 0),
+(4, 20000365, 1),
+(5, 20000321, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -129,9 +140,25 @@ CREATE TABLE `persona` (
   `direccion` varchar(255) DEFAULT NULL,
   `localidad` varchar(255) DEFAULT NULL,
   `correo` varchar(200) DEFAULT NULL,
-  `telefono` int(20) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`dni`, `nombre`, `apellido`, `direccion`, `localidad`, `correo`, `telefono`, `estado`) VALUES
+(20000313, 'Periquita Rocio', 'Lupilo', 'España 123', 'San Luis', 'PeriquitaRL@gmail.com', 2664456963, 1),
+(20000321, 'Sofia', 'Peralta', 'Heroes de Malvina 345', 'San luis', 'SofiaP@gmail.com', 2664232369, 1),
+(20000365, 'Esteban ', 'Fernandez', 'Barranca 345', 'San Luis', 'Esteban@gmail.com', 2664653268, 1),
+(20000999, 'Maximiliano Lucas', 'Ruso Veracruz', 'Pueyrredon 3433', 'Villa Mercedes', 'MaximilianoRV@gmail.com', 2664444666, 1),
+(30000121, 'Fernanda', 'Romero Diaz', NULL, NULL, NULL, 2657875464, 1),
+(30000123, 'Federico Muñoz', 'Rodriguez', 'Pedernera 3432', 'Villa Mercedes', 'FedeR@mail.com', 2657445556, 1),
+(30000212, 'Rocio Tamara', 'Maldonado Urquiza', 'Pedernera 3432', 'Villa Mercedes', 'RocioM@mail.com', 2657562326, 1),
+(30000213, 'Franco', 'Barcheta', 'Moreno 3454', 'Villa Mercedes', 'FedericoB@mail.com', 2657445326, 1),
+(30000459, 'Agustina Rocio', 'Rodriguez', 'Mitre 3434', 'Villa Mercedes', 'Agustinarr@mail.com', 2657446523, 1),
+(50000555, 'Tomas Rodrigo Agustin', 'Sosa Felipe', 'Rivadavia 4565', 'Buenos Aires', 'Tomasras@hotmail.com', 1134565645, 1);
 
 -- --------------------------------------------------------
 
@@ -144,6 +171,18 @@ CREATE TABLE `propietario` (
   `dni` int(11) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `propietario`
+--
+
+INSERT INTO `propietario` (`id`, `dni`, `estado`) VALUES
+(1, 30000123, 0),
+(2, 30000212, 0),
+(3, 30000121, 0),
+(4, 30000213, 0),
+(5, 50000555, 1),
+(6, 30000459, 0);
 
 -- --------------------------------------------------------
 
@@ -188,8 +227,8 @@ ALTER TABLE `contrato`
 --
 ALTER TABLE `gestion`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `entidad_id` (`entidad_id`),
-  ADD KEY `usuario_id` (`usuario_id`) USING BTREE;
+  ADD KEY `usuario_id` (`usuario_id`) USING BTREE,
+  ADD KEY `gestion_ibfk_2` (`entidad_id`);
 
 --
 -- Indices de la tabla `inmueble`
@@ -251,9 +290,57 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `contrato`
+--
+ALTER TABLE `contrato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `gestion`
+--
+ALTER TABLE `gestion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `inmueble`
+--
+ALTER TABLE `inmueble`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `inquilino`
+--
+ALTER TABLE `inquilino`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `multa`
+--
+ALTER TABLE `multa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `propietario`
+--
+ALTER TABLE `propietario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de la tabla `tipo_inmueble`
 --
 ALTER TABLE `tipo_inmueble`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
